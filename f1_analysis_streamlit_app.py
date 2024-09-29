5706fd0725a4e47163f0d749974f1df8e7be2f86
Ashutosh.shukla <113657895+shukla-ashutosh4@users.noreply.github.com>
2024-09-29 21:57:12 +0530

    Update f1_analysis_streamlit_app.py

--- f1_analysis_streamlit_app.py
+++ f1_analysis_streamlit_app.py
@@ -675,7 +675,7 @@ for i, driver in enumerate(winners.index):
 for i in range(len(winners)):
     driver_name = winners.index[i].split()[1]
     color = color_dicttt.get(driver_name, '#bbbbbb')
-    plt.text(winners[i]-1.3, i+0.15, "{:>3}".format(winners[i]), fontsize=19, fontweight='bold', color=color)
+    plt.text(winners[i]-1.3, i+0.15, "{:>3}".format(winners[i]), fontsize=19, fontweight='bold', color='#000000')
     plt.text(winners[i]+0.2, i+0.15, "{:>3}".format(winners[i]), fontsize=19, fontweight='bold', color='k')
 
 plt.title('Formula 1 - 2023 Season\n# of Race Wins (Drivers)', fontsize=19, fontweight='bold', color='#bbbbbb')
